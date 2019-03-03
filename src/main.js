d6770ed420138127b8f19487a10186a9873d3952
Shannon Michaels <michaelsshan@seattleu.edu>
2019-03-02 19:44:34 -0800

    Second Commit. Updated the code to reflect new JavaScript ideals. Made a button to start the fractal generating.

--- src/main.js
+++ src/main.js
@@ -1,55 +1,47 @@
-(function() {
-    // Create Canvas
-    var myCanvas = document.createElement("canvas");
-    myCanvas.width=1000;
-    myCanvas.height=1000;
-    document.body.appendChild(myCanvas);
-    var ctx = myCanvas.getContext("2d");
+// Create Canvas
+let myCanvas = document.createElement("canvas");
+myCanvas.width=1000;
+myCanvas.height=1000;
+document.body.appendChild(myCanvas);
+let ctx = myCanvas.getContext("2d");
 
-    // Start drawing
-    function checkIfBelongsToMandelbrotSet(x,y) {
-        var realComponentOfResult = x;
-        var imaginaryComponentOfResult = y;
-        var maxIterations = 100;
-        for(var i = 0; i < maxIterations; i++) {
-            var tempRealComponent = realComponentOfResult * realComponentOfResult
-                - imaginaryComponentOfResult * imaginaryComponentOfResult
-                + x;
-            var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult
-                + y;
-            realComponentOfResult = tempRealComponent;
-            imaginaryComponentOfResult = tempImaginaryComponent;
+// Start drawing
+function checkIfBelongsToMandelbrotSet(x,y) {
+    let realComponentOfResult = x;
+    let imaginaryComponentOfResult = y;
+    let maxIterations = 100;
+    for(let i = 0; i < maxIterations; i++) {
+        let tempRealComponent = realComponentOfResult * realComponentOfResult
+            - imaginaryComponentOfResult * imaginaryComponentOfResult
+            + x;
+        let tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult
+            + y;
+        realComponentOfResult = tempRealComponent;
+        imaginaryComponentOfResult = tempImaginaryComponent;
 
-            // Return a number as a percentage
-            if(realComponentOfResult * imaginaryComponentOfResult > 5)
-                return (i/maxIterations * 100);
-        }
-        return 0;   // Return zero if in set
+        // Return a number as a percentage
+        if(realComponentOfResult * imaginaryComponentOfResult > 5)
+            return (i/maxIterations * 100);
     }
-    if(belongsToSet == 0) {
-        ctx.fillStyle = '#000';
-        ctx.fillRect(x,y, 1,1); // Draw a black pixel
-    } else {
-        ctx.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
-        ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
-    }
+    return 0;   // Return zero if in set
+}
 
-    var magnificationFactor = 200;
-    var panX = 2;
-    var panY = 1.5;
-    for(var x=0; x < myCanvas.width; x++) {
-        for(var y=0; y < myCanvas.height; y++) {
-            var belongsToSet =
-                checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX,
-                    y/magnificationFactor - panY);
-            if(belongsToSet == 0) {
+function draw() {
+    let magnificationFactor = 200;
+    let panX = 2;
+    let panY = 1.5;
+    for (let x = 0; x < myCanvas.width; x++) {
+        for (let y = 0; y < myCanvas.height; y++) {
+            let belongsToSet =
+                checkIfBelongsToMandelbrotSet(x / magnificationFactor - panX,
+                    y / magnificationFactor - panY);
+            if (belongsToSet === 0) {
                 ctx.fillStyle = '#000';
-                ctx.fillRect(x,y, 1,1); // Draw a black pixel
+                ctx.fillRect(x, y, 1, 1); // Draw a black pixel
             } else {
                 ctx.fillStyle = 'hsl(240, 100%, ' + belongsToSet + '%)';
-                ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
+                ctx.fillRect(x, y, 1, 1); // Draw a colorful pixel
             }
         }
     }
-
-})();
+}
